7ac6eb260a61b68c5e2a6ab91b892b4e5ffe8134
matveenya <66434361+matveenya@users.noreply.github.com>
2020-06-05 18:45:35 +0300

    Add files via upload

---
+++ 3-1.cpp
@@ -0,0 +1,22 @@
+﻿//Определить, сколько слов в строке начинается с прописной буквы. Вывести их на экран.
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+	char str[] = "There are,Five Words/with !Capital letter ?in this Sentence.";
+	char key[] = " .,!?/";
+	int n = 0;
+
+	for (int i = 0; i < strlen(str); i++)
+	{
+		for (int j = 0; j < strlen(key); j++)
+		{
+			if (str[i - 1] == key[j - 1])
+				if ((str[i] >= 65) && (str[i] <= 90))
+					n++;
+		}
+	}
+	cout << n << endl;
+}
