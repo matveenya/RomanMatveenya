6472fba79dfb9b6ff8536f596206052a4640ef69
matveenya <66434361+matveenya@users.noreply.github.com>
2020-06-05 18:46:59 +0300

    Add files via upload

---
+++ 4-1.cpp
@@ -0,0 +1,33 @@
+﻿//Отсортировать строки по количеству слов, содержащих цифры
+#include <iostream>
+#include <cmath>
+#include <fstream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+int main()
+{
+	char s[20];
+
+	std::ifstream ifs("test_numbers.txt");
+	std::ofstream ofs("2.txt");
+	if (ifs && ofs)
+	{
+		for (std::size_t i = 0; std::getline(s, 20, " "); ++i)
+		{
+			if (i % 2 == 0) {
+				ofs << atoi(s) / 2 << '\n';
+			}
+			else {
+				ofs << atoi(s) * 3 + 1 << '\n';
+			}
+
+		}
+	}
+	else std::cerr << "Can't open file\n";
+	ifs.close();
+	ofs.close();
+	return 0;
+
+}
